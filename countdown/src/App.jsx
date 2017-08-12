35a82eb682e472aeeaedfdcafa3a50d1a5bfdeac
Marcio Godoi <souzagodoi@gmail.com>
2017-08-12 15:49:15 -0300

    updating to work with css

--- countdown/src/App.jsx
+++ countdown/src/App.jsx
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import './App.css';
+
 
 class App extends Component {
   render(){
     return (
-      <div> Countdown Champ, App Component</div>
+      <div className="App">
+        <div className="App-title">countdown to December 25, 2017</div>
+        <div>
+          <div className="Clock-days">14 days</div>
+          <div className="Clock-hours">30 hours</div>
+          <div className="Clock-minutes">15 minutes</div>
+          <div className="Clock-seconds">20 seconds</div>
+        </div>
+      </div>
     )
   }
 }
