dc10bce677d8007a16548e9d752a3e6c284e6a4c
Marcio Godoi <souzagodoi@gmail.com>
2017-08-12 15:38:30 -0300

    Adding the first component!

--- countdown/src/index.js
+++ countdown/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import App from './App';
 
 ReactDom.render(
-  <div>countdown champ</div>, document.getElementById('root')
+  <App></App>, document.getElementById('root')
 )
