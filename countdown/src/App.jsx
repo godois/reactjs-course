c7b1a2922b3ddca76a9364e7dbb91204475a9969
Marcio Godoi <souzagodoi@gmail.com>
2017-08-15 22:00:29 -0300

    Formatting the page with boostrap controls

--- countdown/src/App.jsx
+++ countdown/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Clock from './Clock'
+import { Form, FormControl, Button} from 'react-bootstrap'
 
 class App extends Component {
 
@@ -21,10 +22,10 @@ class App extends Component {
       <div className="App">
         <div className="App-title">Countdown to {this.state.deadline}</div>
         <Clock deadline={this.state.deadline}/>
-        <div>
-          <input placeholder='new date' onChange={event => this.setState({newDeadline: event.target.value})}/>
-          <button onClick={() => this.changeDeadline()}> Submit</button>
-        </div>
+        <Form inline>
+          <FormControl className="Deadline-input" placeholder='new date' onChange={event => this.setState({newDeadline: event.target.value})}/>
+          <Button onClick={() => this.changeDeadline()}> Submit</Button>
+        </Form>
       </div>
     )
   }
